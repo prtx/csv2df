00ddad9fdc5ec977f55d0b8487d00cf20ef82d91
sagunji <karanjit.sagun01@gmail.com>
2019-02-26 20:42:00 +0545

    master: Add cors to server

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os
 import json
+from flask_cors import CORS
 from urllib.parse import urlencode
 
 from flask import Flask, jsonify, request, redirect
@@ -12,6 +13,7 @@ UPLOAD_FOLDER = 'uploads'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+CORS(app)
 
 @app.route('/uploader', methods = ['POST'])
 def upload_file():
@@ -21,39 +23,39 @@ def upload_file():
     f = request.files.get("csv")
     if not f:
         return "File not provided.", 400
-    
-    filename, ext = os.path.splitext(f.filename)
-    print(ext)
+
+    fullfilename = secure_filename(f.filename)
+    filename, ext = os.path.splitext(fullfilename)
     if ext != ".csv":
         return "Invalid file type.", 400
 
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename))
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], fullfilename)
     f.save(file_path)
     return redirect("/get/{}?{}".format(filename, request.query_string.decode("utf8")))
 
 
 @app.route("/get/<filename>", methods=["GET"])
 def get_data(filename):
+    print('here')
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename("{}.csv".format(filename)))
-
     delimiter = request.args.get("delimiter", ",")
     try:
         df = pd.read_csv(file_path, delimiter=delimiter)
+        print(df.head())
     except pd.errors.ParserError:
         return "Invalid delimeter.", 400
 
     headers = request.args.get("headers")
     if headers:
         headers = headers.split(",")
-
-    for h in headers:
-        if h not in df.columns:
-            return "Invalid header \"{}\".".format(h), 400
-    df = df[headers]
+        for h in headers:
+            if h not in df.columns:
+                return "Invalid header \"{}\".".format(h), 400
+        df = df[headers]
 
     data = json.loads(df.to_json(orient='records'))
-    return jsonify(data), 200
+    return jsonify(filename=filename, data=data), 200
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8181)
